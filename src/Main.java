7945142c3ecd5f04c69d04ee4e1d2267512e894d
sergeykorn81 <sergeykorn81@gmail.com>
2022-02-02 19:58:11 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+
+        int ticketPrice = 1000;
+        int bonusRubles = 20;
+        int bonusMiles = ticketPrice / bonusRubles;
+        System.out.println(bonusMiles);
+
+    }
+}
+
